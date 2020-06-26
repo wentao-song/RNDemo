6832e96cfb992f9728f2ec767e1b4ad7fc5920ba
宋文涛 <498389182@qq.com>
2020-06-27 00:03:26 +0800

    新建项目

---
+++ components/app1.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {View, Text, Button} from 'react-native';
+
+export default class app1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Button />
+        <Text> app1 </Text>{' '}
+      </View>
+    );
+  }
+}
